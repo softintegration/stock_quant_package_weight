93d96b8edd4f9505739a377e48d4e94bb83808e8
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-03 13:56:15 +0100

    [FIX] Package:Fix issue of removing of the «Weight» value when updating the module

--- __manifest__.py
+++ __manifest__.py
@@ -10,13 +10,12 @@
     'qweb': [],
     'description': False,
     'images': [],
-    'version': '1.0.1.2',
+    'version': '1.0.1.3',
     'category': 'Stock',
     'demo': [],
     'depends': ['stock_quant_package_product_unique','prepress_management'],
     'data': [
         'views/stock_quant_views.xml',
-        'data/gram_weight_data_migration.sql'
     ],
     'license': 'LGPL-3',
 }
--- data/gram_weight_data_migration.sql
+++
@@ -1,8 +0,0 @@
-
---Here we have to get the old data for the field stock_quant_package.gram_weight after the update of the field type from float to integer
-UPDATE stock_quant_package SET gram_weight=gram_weight_moved0;
-
-
-
-
-
